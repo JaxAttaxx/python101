ec639edb94ce5958eb7b4ab71b0b6b9fc10f2f26
Jackson Godwin <jaxjunkmail80@gmail.com>
2020-08-18 18:02:20 -0400

    2 hours later

---
+++ HELLO2.py
@@ -0,0 +1,5 @@
+user_name = input('What is your name? ')
+
+# print('HELLO,', user_name.upper() + '!')
+print(f"HELLO, {user_name.upper()}!")
+print(f"YOUR NAME HAS {len(user_name)} LETTERS IN IT! AWESOME!")
