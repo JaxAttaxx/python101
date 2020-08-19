c7bfc0cc720e04764e67eed9dc5b0b5705509209
Jackson Godwin <jaxjunkmail80@gmail.com>
2020-08-18 22:23:37 -0400

    4 hours later

---
+++ Madlib.py
@@ -0,0 +1,7 @@
+print("Please fill in the blanks below: ")
+
+
+
+name_choice = input("What is name?") 
+subject_choice = input("What is Subject?")
+print(name_choice + "\'s least favorite subject in school is " + subject_choice)
